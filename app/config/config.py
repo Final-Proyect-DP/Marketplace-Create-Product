78b6be2c4de60e74fd8cb5a926f21956d8970892
Fernandonov21 <fernandonovillo21@gmail.com>
2025-02-04 16:37:51 -0500

    feat: :sparkles: Add DATABASE_URI to environment configuration for database connection

--- app/config/config.py
+++ app/config/config.py
@@ -1,8 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        'DATABASE_URI',
-        'mysql+mysqlconnector://admin:FxNc10082002*@marketplace-create.ctymya2qk1r6.us-east-1.rds.amazonaws.com:3306/createdb'
-    )
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
